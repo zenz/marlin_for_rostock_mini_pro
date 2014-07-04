a361793806b89bcab9c065fce6a1c17e13c133b8
Simon Hu <simonhu@airfit.cn>
2014-07-04 10:26:18 +0800

    disable AUTO_BED_LEVELING by default

--- Configuration.h
+++ Configuration.h
@@ -398,7 +398,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define Z_MAX_LENGTH (Z_MAX_POS - Z_MIN_POS)
 //============================= Bed Auto Leveling ===========================
 
-#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
+//#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
 
 #ifdef ENABLE_AUTO_BED_LEVELING
 
